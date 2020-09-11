c47da867fd526677f234802c6d01eba1778f4e44
Simeng Hao <simeng.hao@outlook.com>
2020-09-11 02:31:50 -0400

    load and parse cohort assignment into object

---
+++ idToCohort.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const parse = require('csv-parse/lib/sync');
+let idToCohort = {};
+let load = () => {
+    let file = fs.readFileSync('./asset/F20 CS2110 Cohorts.csv', 'utf-8');
+    // console.log(file);
+    let rows = parse(file, { columns: false, trim: true })
+    rows.forEach((row) => {
+        let id = row[0].trim();
+        if (id === "0") {
+            return
+        }
+        let cohort = row[1]
+        // console.log(`${id}: ${cohort}`)
+        idToCohort[id] = cohort
+        // console.log(idToCohort[id]);
+    })
+}
+
+load();
+module.exports = idToCohort
+
+
